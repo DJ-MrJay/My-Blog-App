227fde14059d652ef8f5785a95b629f084c838ca
Jonah Wambua <djmrjay@gmail.com>
2023-03-30 14:53:51 +0300

    Add schema and refactor code

--- db/migrate/20230329170939_create_comments.rb
+++ db/migrate/20230329170939_create_comments.rb
@@ -1,12 +1,10 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.integer :user_id
-      t.integer :post_id
+      t.references :user, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
       t.text :text
-      t.datetime :updated_at
-      t.datetime :created_at
-
+      
       t.timestamps
     end
   end
