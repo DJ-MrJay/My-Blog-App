7c64e0932d41052a5420092b237577feeb8198e1
Jonah Wambua <djmrjay@gmail.com>
2023-05-11 10:35:52 +0300

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :comments, through: :posts
 
   validates :name, presence: true
+  validates :posts_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
