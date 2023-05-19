2456423268064731b41aa00aaf17e859092af52f
Jonah Wambua <djmrjay@gmail.com>
2023-05-19 14:54:38 +0300

    Remove actions from controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,9 @@
 class PostsController < ApplicationController
   def index
     # Placeholder action
-    @user = User.find(params[:user_id])
-    @posts = @user.posts
   end
 
   def show
     # Placeholder action
-    @user = User.find(params[:user_id])
-    @post = Post.find(params[:id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
   def index
     # Placeholder action
-    @users = User.all
   end
 
   def show
     # Placeholder action
-    @user = User.find(params[:id])
   end
 end
