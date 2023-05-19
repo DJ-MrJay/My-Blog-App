c757f63a14e74c48f21d22fdb7ccd32d7c939eda
Jonah Wambua <djmrjay@gmail.com>
2023-05-19 14:55:56 +0300

    Add controller specs

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,38 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe PostsController, type: :request do
+RSpec.describe '/posts', type: :request do
   describe 'GET /index' do
-    before do
-      get '/posts' # Updated the URL to match the index action of the PostsController
+    before(:example) do
+      get '/users/1/posts'
     end
-
     it 'renders a successful response' do
       expect(response).to be_successful
     end
-
     it 'correctly renders the index template' do
       expect(response).to render_template(:index)
     end
-
-    it 'contains the placeholder text' do
+    it 'contain the placeholder text' do
       expect(response.body).to include('List of Posts')
     end
   end
 
   describe 'GET /show' do
-    before do
-      get '/posts/1' # Updated the URL to match the show action of the PostsController
+    before(:example) do
+      get '/users/1/posts/1'
     end
-
     it 'renders a successful response' do
       expect(response).to be_successful
     end
-
     it 'correctly renders the show template' do
       expect(response).to render_template(:show)
     end
-
-    it 'contains the placeholder text' do
+    it 'contain the placeholder text' do
       expect(response.body).to include('Post Details')
     end
   end
--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,39 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :request do
+RSpec.describe '/users', type: :request do
   describe 'GET /index' do
-    before do
+    before(:example) do
       get '/users'
     end
-
     it 'renders a successful response' do
       expect(response).to be_successful
     end
-
     it 'correctly renders the index template' do
       expect(response).to render_template(:index)
     end
-
-    it 'contains the placeholder text' do
+    it 'contain the placeholder text' do
       expect(response.body).to include('List of Users')
     end
   end
 
   describe 'GET /show' do
-    before do
-      user = User.create(name: 'John Doe') # Creating a user for testing purposes
-      get "/users/#{user.id}"
+    before(:example) do
+      get '/users/1'
     end
-
     it 'renders a successful response' do
       expect(response).to be_successful
     end
-
     it 'correctly renders the show template' do
       expect(response).to render_template(:show)
     end
-
-    it 'contains the placeholder text' do
+    it 'contain the placeholder text' do
       expect(response.body).to include('User Details')
     end
   end
