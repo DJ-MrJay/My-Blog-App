24ec4e674a98416266fc854a11656f1681afe327
Jonah Wambua <djmrjay@gmail.com>
2023-05-20 21:11:08 +0300

    Add controller actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,13 @@
 class PostsController < ApplicationController
   def index
-    # Placeholder action
+    @posts = Post.where(author_id: params[:user_id])
+    @user = User.find(params[:user_id])
   end
 
   def show
-    # Placeholder action
+    @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
+    @comments = @post.comments
+    @likes = @post.likes
   end
 end
