0c7b9cb753421f756cc798b634af9130c5dd9c3d
Jonah Wambua <djmrjay@gmail.com>
2023-05-20 21:12:06 +0300

    Alter models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   belongs_to :post
 
   after_create :update_post_comments_counter
@@ -7,6 +7,6 @@ class Comment < ApplicationRecord
   validates :text, presence: true
 
   def update_post_comments_counter
-    post.update_comments_counter
+    post.update(comments_counter: Comment.where(post_id:).count)
   end
 end
