9ed73784818b833a63fb12b71fd80f3bfcd70450
James Serengia <jamesserengia@gmail.com>
2023-07-26 16:21:45 +0300

    Refactor comments controller to eager load

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,21 +3,17 @@ class CommentsController < ApplicationController
     @comment = Comment.new
     @post = Post.find(params[:post_id])
   end
-
   def create
-    @post = Post.find(params[:post_id])
+    @post = Post.includes(:comments).find(params[:post_id]) # Eager loading comments for the specific post
     @comment = @post.comments.new(comment_params)
     @comment.author = current_user
-
     if @comment.save
       redirect_to user_post_path(current_user, @post)
     else
       render :new, status: :unprocessable_entity
     end
   end
-
   private
-
   def comment_params
     params.require(:comment).permit(:text)
   end
