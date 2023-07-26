f2051c3243f7e20604e73578ffd56a870e07f3f3
Jonah Wambua <djmrjay@gmail.com>
2023-07-26 16:09:35 +0300

    Refactor users controller to eager load

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.all.includes(:posts) # Eager loading posts for all users
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(posts: [{ comments: :author }]).find(params[:id]) # Eager loading posts, comments, and authors for the specific user
   end
 end
