a5db5b25fd23715222c2fb8859c356ba7b30c928
Jonah Wambua <djmrjay@gmail.com>
2023-07-27 15:36:44 +0300

    Removed add_index in migration files

--- db/migrate/20230329170939_create_comments.rb
+++ db/migrate/20230329170939_create_comments.rb
@@ -4,10 +4,8 @@ class CreateComments < ActiveRecord::Migration[7.0]
       t.references :user, null: false, foreign_key: true
       t.references :post, null: false, foreign_key: true
       t.text :text
-      
+
       t.timestamps
     end
-    add_index :comments, :user_id
-    add_index :comments, :post_id
   end
 end
