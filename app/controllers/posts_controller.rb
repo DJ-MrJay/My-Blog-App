b7ab2f5aab4a52c92c8dd8ae97773f81b9930bad
Jonah Wambua <djmrjay@gmail.com>
2023-07-29 04:23:34 +0300

    Implement controller actions for CanCanCan

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  load_and_authorize_resource
   def index
     @user = User.find(params[:user_id])
     # Eager loading comments and likes for the user's posts
@@ -21,14 +23,29 @@ class PostsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @post = @user.posts.new(posts_params)
+    @post.author = @user
 
     if @post.save
       @post.update_user_posts_counter
-      flash[:notice] = 'Your post was created successfully'
+      flash[:notice] = 'Your post was created successfully.'
       redirect_to user_post_path(@user, @post)
     else
-      flash[:notice] = 'Sorry, post was not created successfully'
+      flash[:notice] = 'Sorry, post was not created.'
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    post = Post.find(params[:id])
+    post.comments.destroy_all
+    post.likes.destroy_all
+    if post.destroy
+      flash[:success] = 'Post deleted successfully.'
+      redirect_to users_path
+    else
+      flash.now[:error] = 'Error: Post could not be deleted.'
+      redirect_to users_path
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  load_and_authorize_resource
   def index
     # Eager loading posts for all users
     @users = User.all.includes(:posts)
@@ -8,5 +9,17 @@ class UsersController < ApplicationController
   def show
     # Eager loading posts, comments, and authors for the specific user
     @user = User.includes(posts: [{ comments: :author }]).find(params[:id])
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.posts.destroy_all
+    if user.destroy
+      flash[:success] = 'User deleted successfully.'
+      redirect_to users_path
+    else
+      flash.now[:error] = 'Error: User could not be deleted.'
+      redirect_to user_path(user)
+    end
   end
 end
